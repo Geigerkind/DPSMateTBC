be004b35c27a36a8ea6b84114d8162e44334a69e
Albea <tomdymel@web.de>
2017-01-20 17:40:22 +0100

    Fixed damage doubling issue if the pet has the same name as the player and performance boost

--- modules/DPSMate_Details_DamageTotal.lua
+++ modules/DPSMate_Details_DamageTotal.lua
@@ -337,7 +337,7 @@ function DPSMate.Modules.DetailsDamageTotal:SortLineTable(uid)
 		end
 	end
 	-- Pet
-	if DPSMateUser[user][5] and DPSMateUser[DPSMateUser[user][5]] and DPSMateSettings["mergepets"] then
+	if DPSMateUser[user][5] and DPSMateUser[DPSMateUser[user][5]] and DPSMateSettings["mergepets"] and DPSMateUser[user][5]~=user then
 		if db[DPSMateUser[DPSMateUser[user][5]][1]] then
 			for cat, val in pairs(db[DPSMateUser[DPSMateUser[user][5]][1]]) do
 				if cat~="i" and val["i"] then
