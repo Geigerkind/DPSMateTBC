021b131fdfad214b0ef0eed324e9282fe536ffbe
Albea <tomdymel@web.de>
2017-01-29 19:37:05 +0100

    Few bugfixes

--- DPSMate/DPSMate.lua
+++ DPSMate/DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 35
+DPSMate.VERSION = 36
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
