ec78ffb19046659d4171e4c127135462396f1edb
Albea <tomdymel@web.de>
2017-03-03 15:59:38 +0100

    Build 37

--- DPSMate/DPSMate.lua
+++ DPSMate/DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 36
+DPSMate.VERSION = 37
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
