301abfa3cf24ef90d7e586ffa257df86b7292246
Albea <tomdymel@web.de>
2017-03-06 22:59:40 +0100

    Fixing typo <.<

--- DPSMate/DPSMate.lua
+++ DPSMate/DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 40
+DPSMate.VERSION = 41
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
