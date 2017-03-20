b67a3fd124c4bb554c37f380172184ec95e73095
Albea <tomdymel@web.de>
2017-03-20 11:53:42 +0100

    Typo3

--- DPSMate/DPSMate_Options.lua
+++ DPSMate/DPSMate_Options.lua
@@ -1682,7 +1682,7 @@ function DPSMate.Options:NewSegment(segname)
 		--DPSMateThreat[2] = {}
 		DPSMateFails[2] = {}
 		DPSMateCCBreaker[2] = {}
-		DPSMateCombatTime["current"] = 1
+		DPSMateCombatTime["current"] = 0.0001
 		DPSMateCombatTime["effective"][2] = {}
 		DPSMate:SetStatusBarValue()
 	end
